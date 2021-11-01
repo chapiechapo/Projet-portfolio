8d793e3172af081554bf942a2e3f7f5ce4c46918
Jessica Bordat <bordat.jessica71@gmail.com>
2021-11-01 11:56:08 +0100

    modification du php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 
-include_once("index.html, a propos.html, competences.html, contact.html");
+include_once("index.html");
+include_once("a propos.html");
+include_once("competence.html");
+include_once("contact.html");
+?>
